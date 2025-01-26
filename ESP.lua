4553e9a3d292acb979674272528d019907c58d5b
Ele5140 <128095467+Ele5140@users.noreply.github.com>
2025-01-25 22:13:02 -0500

    Update ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -1,80 +1,135 @@
--- Script by Ele5140 on GitHub, if you get access to it, you may use it. (idk how copy stuff works lmao)
+--[[ 
+
+Script by Ele5140 on GitHub.
+I didn't consider devices with screens smaller/larger than my laptop's, I'm sorry lmao :sob:
+
+]]
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local UIS = game:GetService("UserInputService")
+local TS = game:GetService("TweenService")
 
 local Bypass = game:GetService("CoreGui")
 
 local CoreGui = Instance.new("Folder")
 CoreGui.Parent = Bypass
 
+local Colors = {}
+Colors.Players = Color3.new(1,1,1)
+Colors.CHAIN = Color3.new(1,0.4,0.4)
+
+-- UI
+
+local MainUI = Instance.new("ScreenGui")
+MainUI.Parent = CoreGui
+MainUI.Enabled = true
+
+local UI = Instance.new("Frame")
+UI.Parent = MainUI
+UI.Size = UDim2.new(0.983,0,0.966,0)
+UI.Position = UDim2.new(0.008,0,0.016,0)
+UI.BackgroundTransparency = 1
+
+local Layout = Instance.new("UIGridLayout")
+Layout.Parent = UI
+Layout.CellPadding = UDim2.new(0,10,0,5)
+Layout.CellSize = UDim2.new(0,50,0,50)
+Layout.HorizontalAlignment = Enum.HorizontalAlignment.Right
+Layout.VerticalAlignment = Enum.VerticalAlignment.Bottom
+
+local PlayersButton = Instance.new("ImageButton")
+PlayersButton.Parent = UI
+PlayersButton.BackgroundTransparency = 0.5
+PlayersButton.BackgroundColor3 = Color3.fromRGB(46, 49, 72)
+PlayersButton.Image = "rbxassetid://18112836267"
+local Round1 = Instance.new("UICorner")
+Round1.CornerRadius = UDim.new(1,0)
+Round1.Parent = PlayersButton
+
+local CHAINButton = Instance.new("ImageButton")
+CHAINButton.Parent = UI
+CHAINButton.BackgroundTransparency = 0.5
+CHAINButton.BackgroundColor3 = Color3.fromRGB(46, 49, 72)
+CHAINButton.Image = "rbxassetid://13791057421"
+local Round2 = Instance.new("UICorner")
+Round2.CornerRadius = UDim.new(1,0)
+Round2.Parent = CHAINButton
+
+-- Highlight Creation
+
 local create1 = function(thing, name, color)
 	local highlight = Instance.new("Highlight")
 	highlight.Adornee = thing
 	highlight.Parent = CoreGui
-	highlight.Enabled = false
 	highlight.FillTransparency = 1
 	highlight.OutlineColor = color
 	highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
 	highlight.Name = name
 end
 
-local Toggle = false
-local ChainToggle = false
+local Toggle = true
+local ChainToggle = true
+
+PlayersButton.MouseButton1Down:Connect(function()
+	Toggle = not Toggle
+end)
+
+CHAINButton.MouseButton1Down:Connect(function()
+	ChainToggle = not ChainToggle
+end)
 
 UIS.InputBegan:Connect(function(input, v)
 	if v then return end
-	if input.KeyCode ==Enum.KeyCode.P then
-		Toggle = not Toggle
-	end
-
-	if input.KeyCode ==Enum.KeyCode.O then
-		ChainToggle = not ChainToggle
+	
+	if input.KeyCode == Enum.KeyCode.K then
+		MainUI.Enabled = not MainUI.Enabled
 	end
 end)
 
+-- Highlight Handler
+
 for i, player in pairs(Players:GetChildren()) do
 	repeat wait() until player.Character
-	if not CoreGui:FindFirstChild(player.UserId .. "ESP1") then
-		create1(player.Character, (player.UserId .. "ESP1"), Color3.new(1,1,1))
+	if not CoreGui:FindFirstChild(player.UserId) then
+		create1(player.Character, (player.UserId), Color3.new(1,1,1))
 		task.wait()
 	end
+	
 end
 
 game.Players.PlayerAdded:Connect(function(player)
 	repeat wait() until player.Character
-	if not CoreGui:FindFirstChild(player.UserId .. "ESP1") then
-		create1(player.Character, (player.UserId .. "ESP1"), Color3.new(1,1,1))
+	if not CoreGui:FindFirstChild(player.UserId) then
+		create1(player.Character, (player.UserId), Color3.new(1,1,1))
 	end
 end)
 
 game.Players.PlayerRemoving:Connect(function(player)
-	CoreGui:FindFirstChild(player.UserId .. "ESP1"):Destroy()
+	if CoreGui:FindFirstChild(player.UserId) then
+		CoreGui:FindFirstChild(player.UserId):Destroy()
+	end		
 end)
 
 RunService.Heartbeat:Connect(function()
 	for i, player in pairs(Players:GetChildren()) do
 		repeat wait() until player.Character
-		if not CoreGui:FindFirstChild(player.UserId .. "ESP1") then
-			create1(player.Character, (player.UserId .. "ESP1"), Color3.new(1,1,1))
-			task.wait()
-		end
-
-		for i, v in pairs(CoreGui:GetChildren()) do
-			if v:IsA("Highlight") and v.Name == (player.UserId .. "ESP1") then
-				if player.Character:FindFirstChild("Humanoid") then
-					if player.Character:FindFirstChild("Humanoid").Health ~= 0  then
-						v.Enabled = Toggle
-					else
-						v.Enabled = false
-					end
-				end
+		if player.Character then
+			if not CoreGui:FindFirstChild(player.UserId) then
+				create1(player.Character, player.UserId, Colors.Players)
 			end
+			CoreGui:FindFirstChild(player.UserId).Enabled = Toggle
+			CoreGui:FindFirstChild(player.UserId).Adornee = player.Character
+			
+			if player.Character:WaitForChild("Humanoid").Health ~= 0 then
+				CoreGui:FindFirstChild(player.UserId).OutlineColor = Colors.Players
+				TS:Create(CoreGui:FindFirstChild(player.UserId), TweenInfo.new(0.2), {OutlineTransparency = 0}):Play()
+			end
+			player.Character:WaitForChild("Humanoid").Died:Connect(function()
+				CoreGui:FindFirstChild(player.UserId).OutlineColor = Color3.new(1,0.2,0.2)
+				TS:Create(CoreGui:FindFirstChild(player.UserId), TweenInfo.new(1), {OutlineTransparency = 1}):Play()
+			end)
 		end
-
-		CoreGui:FindFirstChild(player.UserId .. "ESP1").Adornee = player.Character
-
 	end
 end)
 
@@ -84,7 +139,7 @@ RunService.Heartbeat:Connect(function()
 			if workspace.Misc.AI:FindFirstChild("CHAIN") then
 				local chain = workspace.Misc.AI:FindFirstChild("CHAIN")
 				if not CoreGui:FindFirstChild("CHAIN") then
-					create1(chain, "CHAIN", Color3.fromRGB(255, 76, 76))
+					create1(chain, "CHAIN", Colors.CHAIN)
 				end
 				CoreGui:FindFirstChild("CHAIN").Enabled = ChainToggle
 			else
